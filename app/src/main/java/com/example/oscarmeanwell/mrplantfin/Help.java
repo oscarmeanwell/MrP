0e3fa06d12b8f81e1382918d93b08e818fd0890d
Oscar Meanwell <oscar.meanwell@gmail.com>
2018-04-16 19:10:36 +0100

    Help optimized

--- app/src/main/java/com/example/oscarmeanwell/mrplantfin/Help.java
+++ app/src/main/java/com/example/oscarmeanwell/mrplantfin/Help.java
@@ -1,10 +1,7 @@
 package com.example.oscarmeanwell.mrplantfin;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class Help extends AppCompatActivity {
 
@@ -16,30 +13,31 @@ public class Help extends AppCompatActivity {
         setContentView(R.layout.activity_help);
         title = (TextView)findViewById(R.id.titleq);
         content = (TextView)findViewById(R.id.txtShow);
-        title.setText("Help: "+getIntent().getStringExtra("type") + " Plants");
+        String plant = getIntent().getStringExtra("type");
+        title.setText("Help: "+plant + " Plants");
 
-        Double tm = ViewPlant.levels.get(getIntent().getStringExtra("type")).get("temp").get("min");
-        Double tmm = ViewPlant.levels.get(getIntent().getStringExtra("type")).get("temp").get("max");
-        Double ti = ViewPlant.levels.get(getIntent().getStringExtra("type")).get("temp").get("ideal");
+        Double tm = ViewPlant.levels.get(plant).get("temp").get("min");
+        Double tmm = ViewPlant.levels.get(plant).get("temp").get("max");
+        Double ti = ViewPlant.levels.get(plant).get("temp").get("ideal");
 
-        Double hm = ViewPlant.levels.get(getIntent().getStringExtra("type")).get("hum").get("min");
-        Double hmm = ViewPlant.levels.get(getIntent().getStringExtra("type")).get("hum").get("max");
-        Double hi = ViewPlant.levels.get(getIntent().getStringExtra("type")).get("hum").get("ideal");
+        Double hm = ViewPlant.levels.get(plant).get("hum").get("min");
+        Double hmm = ViewPlant.levels.get(plant).get("hum").get("max");
+        Double hi = ViewPlant.levels.get(plant).get("hum").get("ideal");
 
-        Double sm = ViewPlant.levels.get(getIntent().getStringExtra("type")).get("soil").get("min");
-        Double smm = ViewPlant.levels.get(getIntent().getStringExtra("type")).get("soil").get("max");
-        Double si = ViewPlant.levels.get(getIntent().getStringExtra("type")).get("soil").get("ideal");
+        Double sm = ViewPlant.levels.get(plant).get("soil").get("min");
+        Double smm = ViewPlant.levels.get(plant).get("soil").get("max");
+        Double si = ViewPlant.levels.get(plant).get("soil").get("ideal");
 
 
-        String Temp = getIntent().getStringExtra("type") + " Temperature" + System.lineSeparator()
+        String Temp = plant + " Temperature" + System.lineSeparator()
                 + "\t\tMin: " + tm.toString() + System.lineSeparator()
                 + "\t\tMax: " + tmm.toString() + System.lineSeparator()
                 + "\t\tIdeal: "  + ((Double) (ti-5)).toString() + " - " + ((Double) (ti+5)).toString() + System.lineSeparator() + System.lineSeparator()
-                +  getIntent().getStringExtra("type") + " Humidity" + System.lineSeparator()
+                +  plant + " Humidity" + System.lineSeparator()
                 + "\t\tMin: " + hm.toString() + System.lineSeparator()
                 + "\t\tMax: " + hmm.toString() + System.lineSeparator()
                 + "\t\tIdeal: "  + ((Double) (hi-5)).toString() + " - " + ((Double) (hi+5)).toString() + System.lineSeparator() + System.lineSeparator()
-                +  getIntent().getStringExtra("type") + " Soil Humidity" + System.lineSeparator()
+                +  plant + " Soil Humidity" + System.lineSeparator()
                 + "\t\tMin: " + sm.toString() + System.lineSeparator()
                 + "\t\tMax: " + smm.toString() + System.lineSeparator()
                 + "\t\tIdeal: "  + ((Double) (si-5)).toString() + " - " + ((Double) (si+5)).toString() + System.lineSeparator() + System.lineSeparator()
